8778c29b3510538585fdf32c49e6333134fdd648
MSGroupFM <admin@msgru.com>
2022-04-01 02:27:51 +0300

    Refactoring ImageDocument.php

--- Document/ImageDocument.php
+++ Document/ImageDocument.php
@@ -2,67 +2,76 @@
 
 namespace Msgframework\Lib\Document;
 
+use Msgframework\Lib\Application\WebApplication;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
  * ImageDocument class, provides an easy interface to output image data
  *
- * @since  1.1.0
+ * @since  1.0.0
+ * @todo Need create Image parameter for use in render and refactoring render method
  */
 class ImageDocument extends Document
 {
 	/**
 	 * Class constructor
 	 *
-	 * @param   array  $options  Associative array of options
-	 *
-	 * @since  1.0.0
-	 */
-    public function __construct(FactoryInterface $factory, array $options = array())
+     * @param FactoryInterface $factory  Factory
+     * @param WebApplication $application  WebApplication
+     * @param array $options  Associative array of options
+     *
+     * @since  1.0.0
+     */
+    public function __construct(FactoryInterface $factory, WebApplication $application, array $options = array())
     {
-        parent::__construct($factory, $options);
+        parent::__construct($factory, $application, $options);
 
 		// Set mime type
-		$this->_mime = 'image/png';
+		$this->setMimeEncoding('image/png');
 
 		// Set document type
-		$this->_type = 'image';
+		$this->setType('image');
 	}
 
-	/**
-	 * Render the document.
-	 *
-	 * @param boolean $cache   If true, cache the output
-	 * @param array $params  Associative array of attributes
-	 *
-	 * @return  string  The rendered data
-	 *
-	 * @since  1.0.0
-	 */
+    /**
+     * Render the document.
+     *
+     * @param boolean $cache If true, cache the output
+     * @param array $params Associative array of attributes
+     *
+     * @return  string  The rendered data
+     *
+     * @throws \Exception
+     * @since  1.0.0
+     */
 	public function render(bool $cache = false, array $params = array()): Response
 	{
 		// Get the image type
-		$type = $this->factory->getApplication()->input->get('type', 'png');
+        $app = $this->getApplication();
+        $config = $app->getConfig();
+		$type = $config->get('type', 'png');
 
 		switch ($type)
 		{
 			case 'jpg':
 			case 'jpeg':
-				$this->_mime = 'image/jpeg';
+				$this->setMimeEncoding('image/jpeg');
 				break;
 			case 'gif':
-				$this->_mime = 'image/gif';
+                $this->setMimeEncoding('image/gif');
 				break;
 			case 'png':
 			default:
-				$this->_mime = 'image/png';
+                $this->setMimeEncoding('image/png');
 				break;
 		}
 
 		$this->_charset = null;
 
-		parent::render($cache, $params);
+		$response = parent::render($cache, $params);
 
-		return $this->getBuffer();
+        $response->setContent('');
+
+		return $response;
 	}
 }
