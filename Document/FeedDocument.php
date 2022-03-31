790ae7d71ec094e22d1e2e9c65a34f8547a16353
MSGroupFM <admin@msgru.com>
2022-03-31 18:19:15 +0300

    Refactoring FeedDocument render method

--- Document/FeedDocument.php
+++ Document/FeedDocument.php
@@ -2,10 +2,12 @@
 
 namespace Msgframework\Lib\Document;
 
+use Msgframework\Lib\Date\Date;
 use Msgframework\Lib\Document\Feed\FeedImage;
 use Msgframework\Lib\Document\Feed\FeedItem;
 use Msgframework\Lib\Language\Text;
 use Msgframework\Lib\Application\WebApplication;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * FeedDocument class, provides an easy interface to parse and display any feed document
@@ -205,14 +207,16 @@ class FeedDocument extends Document
 	public function render(bool $cache = false, array $params = array()): Response
 	{
 		// Get the feed type
-		$type = Cms::getApplication()->input->get('type', 'rss');
+        $app = $this->getApplication();
+        $request = $app->getFactory()->getRequest();
+		$type = $request->get('type', 'rss');
 
 		// Instantiate feed renderer and set the mime encoding
 		$renderer = $this->loadRenderer(($type) ? $type : 'rss');
 
 		if (!($renderer instanceof DocumentRenderer))
 		{
-			throw new \Exception(Text::_('JGLOBAL_RESOURCE_NOT_FOUND'), 404);
+			throw new \Exception(Text::_('GLOBAL_RESOURCE_NOT_FOUND'), 404);
 		}
 
 		$this->setMimeEncoding($renderer->getContentType());
@@ -220,20 +224,21 @@ class FeedDocument extends Document
 		// Output
 		// Generate prolog
 		$data = "<?xml version=\"1.0\" encoding=\"" . $this->_charset . "\"?>\n";
-		$data .= "<!-- generator=\"" . $this->getGenerator() . "\" -->\n";
 
-		// Generate stylesheet links
-		foreach ($this->_styleSheets as $src => $attr)
-		{
-			$data .= "<?xml-stylesheet href=\"$src\" type=\"" . $attr['type'] . "\"?>\n";
-		}
+//		// Generate stylesheet links
+//		foreach ($this->_styleSheets as $src => $attr)
+//		{
+/*			$data .= "<?xml-stylesheet href=\"$src\" type=\"" . $attr['type'] . "\"?>\n";*/
+//		}
 
 		// Render the feed
 		$data .= $renderer->render();
 
-		parent::render($cache, $params);
+		$response = parent::render($cache, $params);
 
-		return $data;
+        $response->setContent($data);
+
+		return $response;
 	}
 
 	/**
