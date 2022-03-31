87388ae5a988305530a429f1b8046fd995a1d991
MSGroupFM <admin@msgru.com>
2022-03-31 18:25:28 +0300

    Change version HeadRenderer class Refactoring render method in HeadRenderer

--- Document/Renderer/Html/HeadRenderer.php
+++ Document/Renderer/Html/HeadRenderer.php
@@ -2,34 +2,31 @@
 
 namespace Msgframework\Lib\Document\Renderer\Html;
 
-
-
 use Msgframework\Lib\Document\DocumentRenderer;
 
 /**
  * HTML document renderer for the document `<head>` element
  *
- * @since  1.1.0
+ * @since  1.0.0
  */
 class HeadRenderer extends DocumentRenderer
 {
-	/**
-	 * Renders the document head and returns the results as a string
-	 *
-	 * @param   string  $head     (unused)
-	 * @param   array   $params   Associative array of values
-	 * @param   string  $content  The script
-	 *
-	 * @return  string  The output of the script
-	 *
-	 * @since  1.0.0
-	 */
-	public function render($head, $params = array(), $content = null)
+    /**
+     * Renders the document head and returns the results as a string
+     *
+     * @param string $name
+     * @param array|null $params Associative array of values
+     * @param string|null $content The script
+     *
+     * @return  string  The output of the script
+     *
+     * @since  1.0.0
+     */
+	public function render(string $name, ?array $params = array(), ?string $content = null): string
 	{
-		$buffer  = '';
-		$buffer .= $this->_doc->loadRenderer('metas')->render($head, $params, $content);
-		$buffer .= $this->_doc->loadRenderer('styles')->render($head, $params, $content);
-		$buffer .= $this->_doc->loadRenderer('scripts')->render($head, $params, $content);
+        $buffer = $this->_doc->loadRenderer('metas')->render($name, $params, $content);
+		$buffer .= $this->_doc->loadRenderer('styles')->render($name, $params, $content);
+		$buffer .= $this->_doc->loadRenderer('scripts')->render($name, $params, $content);
 
 		return $buffer;
 	}
