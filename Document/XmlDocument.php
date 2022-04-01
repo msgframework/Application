a7c07e566b68d49a57c3dd17e52def2aae6edaf9
MSGroupFM <admin@msgru.com>
2022-04-01 16:51:35 +0300

    Refactoring XmlDocument

--- Document/XmlDocument.php
+++ Document/XmlDocument.php
@@ -2,20 +2,30 @@
 
 namespace Msgframework\Lib\Document;
 
+use Msgframework\Lib\Application\WebApplication;
+use Symfony\Component\HttpFoundation\Response;
+
 /**
  * XmlDocument class, provides an easy interface to parse and display XML output
  *
- * @since  1.1.0
+ * @since  1.0.0
  */
 class XmlDocument extends Document
 {
+    /**
+     * XmlDocument full URL
+     *
+     * @var    string
+     * @since  1.0.0
+     */
+    public string $link = '';
 	/**
 	 * Document name
 	 *
 	 * @var    string
 	 * @since  1.0.0
 	 */
-	protected $name = 'xml';
+	protected string $name = 'XmlDocument';
 
 	/**
 	 * Flag indicating the document should be downloaded (Content-Disposition = attachment) versus displayed inline
@@ -23,45 +33,48 @@ class XmlDocument extends Document
 	 * @var    boolean
 	 * @since  1.0.0
 	 */
-	protected $isDownload = false;
+	protected bool $isDownload = false;
 
 	/**
 	 * Class constructor
 	 *
-	 * @param   array  $options  Associative array of options
-	 *
-	 * @since  1.0.0
-	 */
-    public function __construct(FactoryInterface $factory, array $options = array())
+     * @param FactoryInterface $factory  Factory
+     * @param WebApplication $application  WebApplication
+     * @param array $options  Associative array of options
+     *
+     * @since  1.0.0
+     */
+    public function __construct(FactoryInterface $factory, WebApplication $application, array $options = array())
     {
-        parent::__construct($factory, $options);
+        parent::__construct($factory, $application, $options);
 
 		// Set mime type
-		$this->_mime = 'application/xml';
+		$this->setMimeEncoding('application/xml');
 
 		// Set document type
-		$this->_type = 'xml';
+		$this->setType('xml');
 	}
 
-	/**
-	 * Render the document.
-	 *
-	 * @param boolean $cache   If true, cache the output
-	 * @param array $params  Associative array of attributes
-	 *
-	 * @return  string  The rendered data
-	 *
-	 * @since  1.0.0
-	 */
-	public function render(bool $cache = false, array $params = array()): string
+    /**
+     * Render the document.
+     *
+     * @param boolean $cache If true, cache the output
+     * @param array $params Associative array of attributes
+     *
+     * @return Response The rendered data
+     *
+     * @throws \Exception
+     * @since  1.0.0
+     */
+	public function render(bool $cache = false, array $params = array()): Response
 	{
-		parent::render($cache, $params);
+		$response = parent::render($cache, $params);
 
-		$disposition = $this->isDownload ? 'attachment' : 'inline';
+		$disposition = $this->isDownload() ? 'attachment' : 'inline';
 
-		$this->application->setHeader('Content-disposition', $disposition . '; filename="' . $this->getName() . '.xml"', true);
+        $response->headers->set('Content-disposition', $disposition . '; filename="' . $this->getName() . '.xml"', true);
 
-		return $this->getBuffer();
+		return $response;
 	}
 
 	/**
@@ -71,35 +84,35 @@ class XmlDocument extends Document
 	 *
 	 * @since  1.0.0
 	 */
-	public function getName()
-	{
+	public function getName(): string
+    {
 		return $this->name;
 	}
 
 	/**
 	 * Sets the document name
 	 *
-	 * @param   string  $name  Document name
+	 * @param string $name  Document name
 	 *
-	 * @return  XmlDocument instance of $this to allow chaining
+	 * @return $this instance of $this to allow chaining
 	 *
 	 * @since  1.0.0
 	 */
-	public function setName($name = 'joomla')
-	{
+	public function setName(string $name = 'XmlDocument'): self
+    {
 		$this->name = $name;
 
 		return $this;
 	}
 
-	/**
-	 * Check if this document is intended for download
-	 *
-	 * @return  string
-	 *
-	 * @since  1.0.0
-	 */
-	public function isDownload()
+    /**
+     * Check if this document is intended for download
+     *
+     * @return bool
+     *
+     * @since  1.0.0
+     */
+	public function isDownload(): bool
 	{
 		return $this->isDownload;
 	}
@@ -107,16 +120,44 @@ class XmlDocument extends Document
 	/**
 	 * Sets the document's download state
 	 *
-	 * @param   boolean  $download  If true, this document will be downloaded; if false, this document will be displayed inline
+	 * @param boolean $download  If true, this document will be downloaded; if false, this document will be displayed inline
 	 *
-	 * @return  XmlDocument instance of $this to allow chaining
+	 * @return $this instance of $this to allow chaining
 	 *
 	 * @since  1.0.0
 	 */
-	public function setDownload($download = false)
+	public function setDownload(bool $download = false): self
 	{
 		$this->isDownload = $download;
 
 		return $this;
 	}
+
+    /**
+     * Sets the document link
+     *
+     * @param   string  $url  A url
+     *
+     * @return  self
+     *
+     * @since  1.0.0
+     */
+    public function setLink(string $url): self
+    {
+        $this->link = $url;
+
+        return $this;
+    }
+
+    /**
+     * Returns the document base url
+     *
+     * @return string
+     *
+     * @since  1.0.0
+     */
+    public function getLink(): string
+    {
+        return $this->link;
+    }
 }
